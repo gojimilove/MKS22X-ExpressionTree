0a55687064aa8f40a63782bc54990f7f6714b1e1
Jessica Kim <jkim00@stuy.edu>
2019-04-09 11:28:30 -0400

    filled in other methods from notebook

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -51,35 +51,56 @@ public class ExpressionTree{
   /*return the expression as an infix notation string with parenthesis*/
   /* The sample tree would be: "(3 + (2 * 10))"     */
   public String toString(){
-    /*you are to write this method*/
-    return "";
+    String s = "";
+    if (isValue()) s += getValue();
+    else {
+      s += getLeft().toString();
+      s += getOp();
+      s += getRight().toString();
+    }
+    return s;
   }
 
   /*return the expression as a postfix notation string without parenthesis*/
   /* The sample tree would be: "3 2 10 * +"     */
   public String toStringPostfix(){
-    /*you are to write this method*/
-    return "";
+    String s = "";
+    if (isValue()) s += getValue();
+    else {
+      s += getLeft().toString();
+      s += getRight().toString();
+      s += getOp();
+    }
+    return s;
   }
 
   /*return the expression as a prefix notation string without parenthesis*/
   /* The sample tree would be: "+ 3 * 2 10"     */
 
   public String toStringPrefix(){
-    /*you are to write this method*/
-    return "";
+    String s = "";
+    if (isValue()) s += getValue();
+    else {
+      s += getOp();
+      s += getLeft().toString();
+      s += getRight().toString();
+    }
+    return s;
   }
 
   /*return the value of the specified expression tree*/
   public double evaluate(){
-    /*you are to write this method*/
-    return 0.0;
+    if (isValue()) return getValue();
+    else return apply(getOp(), getLeft().evaluate(), getRight.evaluate());
   }
 
   /*use the correct operator on both a and b, and return that value*/
   private double apply(char op, double a, double b){
-    /*you are to write this method*/
-    return 0.0;
+    if (op == '+') return a + b;
+    else if (op == '-') return a - b;
+    else if (op == '*') return a * b;
+    else if (op == '/') return a / b;
+    else if (op == '%') return a % b;
   }
-  
+
 }
